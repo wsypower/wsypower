1920c097278947b0a905cedc83a98faf6bf2fc8a
nancy12138 <mxj198701@126.com>
2020-07-23 15:14:30 +0800

    普陀大屏接口接入

---
+++ src/views/specializedAnalysis/specializedAnalysis.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="specialized-analysis-page" flex="main:center cross:center">
+    <span class="text">正在开发，敬请期待...</span>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+export default {
+  name: 'specializedAnalysis'
+}
+</script>
+<style lang="scss" scoped>
+  .specialized-analysis-page{
+    height: 100%;
+    width: 100%;
+    .text{
+      font-size: 24px;
+      color: #00c0ff;
+    }
+  }
+</style>
